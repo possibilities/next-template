17843bdc6cb627e41810636e67ff7bb590b64ecd
Mike Bannister <notimpossiblemike@gmail.com>
2020-12-05 22:40:33 -0500

    Forward

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,7 +24,7 @@ const App = (props: AppProps): ReactNode => {
           name='viewport'
           content='minimum-scale=1, initial-scale=1, width=device-width'
         />
-        <title>Next Template</title>
+        <title>Quote App UI Client</title>
       </Head>
       <MaterialUiThemeProvider theme={theme}>
         <CssBaseline />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
-import React, { ReactNode, useState } from 'react'
+import React, { ReactElement, useState } from 'react'
 import Typography from '@material-ui/core/Typography'
 import Box from '@material-ui/core/Box'
-import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import RunIcon from '@material-ui/icons/DirectionsRun'
 import { v4 as uuid } from 'uuid'
 import request from 'axios'
 import isEmpty from 'lodash/isEmpty'
 import isObject from 'lodash/isObject'
 import traverse from 'traverse'
 import first from 'lodash/first'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { vs as codeStyle } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
 
 const apiUrl = 'http://bff-quote-app.itgcloud.vtmutins.com/v1/bff-qa-portal'
+
+const apiQuoteFromUiQuote = (x: unknown) => x
+const uiQuoteFromApiQuote = (x: unknown) => x
 
 const apiOptions = {
   apiUrl,
@@ -17,221 +23,188 @@ const apiOptions = {
   transactionId: uuid(),
 }
 
-const newStaticQuoteProperties = {
-  lobCd: 'HOME',
-  policyTerm: 1,
-  policyTermUnit: 'YR',
-  quoteStatus: 'QuoteIncomplete',
-  systemOfOriginVendor: 'Vermont Mutual',
-  systemOfOriginProduct: 'FrontEnd',
-  systemOfOriginVersion: '2.1',
-}
-
-const prepareNewQuote = (newQuote: object, userId: string) => {
-  const newQuoteProperties = {
-    createdBy: userId,
-    updatedBy: userId,
-  }
-
-  const quote = {
-    ...newQuote,
-    ...newStaticQuoteProperties,
-    ...newQuoteProperties,
-  }
-
-  return quote
-}
-
 const cleanOutput = (obj: unknown) =>
   traverse(obj).map(function (value) {
     if (value === null) this.delete()
     if (value === undefined) this.delete()
-    if (Array.isArray(value || isObject(value)) && isEmpty(value)) {
-      this.delete()
-    }
+    // if (Array.isArray(value || isObject(value)) && isEmpty(value)) {
+    //   this.delete()
+    // }
     if (first(this.path) === 'attachments') this.delete()
     if (this.key?.startsWith('systemOfOrigin')) this.delete()
+    if (this.key === 'runDt') this.delete()
+    if (this.key === 'userId') this.delete()
+    if (this.key === 'transactionId') this.delete()
     if (this.key === 'createdDttime') this.delete()
     if (this.key === 'createdBy') this.delete()
     if (this.key === 'updatedDttime') this.delete()
     if (this.key === 'updatedBy') this.delete()
   })
 
-const Page = (): ReactNode => {
+const Example = ({
+  name,
+  onRun,
+  uiRequest,
+  uiResponse,
+  apiRequest,
+  apiResponse,
+}: {
+  name: string
+  onRun: () => void
+  uiRequest: unknown
+  uiResponse: unknown
+  apiRequest: unknown
+  apiResponse: unknown
+}): ReactElement => {
+  return (
+    <>
+      <Box paddingBottom={4}>
+        <Box paddingBottom={2} display='flex' alignItems='center'>
+          <Box paddingRight={2}>
+            <Typography variant='h1'>{name}</Typography>
+          </Box>
+          <IconButton onClick={onRun}>
+            <RunIcon />
+          </IconButton>
+        </Box>
+        <Box display='flex'>
+          {uiRequest && (
+            <Box paddingBottom={2} paddingRight={4}>
+              <Typography>ui request</Typography>
+              <SyntaxHighlighter language='json' style={codeStyle}>
+                {JSON.stringify(cleanOutput(uiRequest), null, 2)}
+              </SyntaxHighlighter>
+            </Box>
+          )}
+          {apiRequest && (
+            <Box paddingBottom={2} paddingRight={4}>
+              <Typography>api request</Typography>
+              <SyntaxHighlighter language='json' style={codeStyle}>
+                {JSON.stringify(cleanOutput(apiRequest), null, 2)}
+              </SyntaxHighlighter>
+            </Box>
+          )}
+          {apiResponse && (
+            <Box paddingBottom={2} paddingRight={4}>
+              <Typography>api response</Typography>
+              <SyntaxHighlighter language='json' style={codeStyle}>
+                {JSON.stringify(cleanOutput(apiResponse), null, 2)}
+              </SyntaxHighlighter>
+            </Box>
+          )}
+          {uiResponse && (
+            <Box paddingBottom={2} paddingRight={4}>
+              <Typography>ui response</Typography>
+              <SyntaxHighlighter language='json' style={codeStyle}>
+                {JSON.stringify(cleanOutput(uiResponse), null, 2)}
+              </SyntaxHighlighter>
+            </Box>
+          )}
+        </Box>
+      </Box>
+    </>
+  )
+}
+
+const Page = (): ReactElement => {
   const [uiRequests, setUiRequests] = useState<unknown[]>([])
   const [uiResponses, setUiResponses] = useState<unknown[]>([])
   const [apiRequests, setApiRequests] = useState<unknown[]>([])
   const [apiResponses, setApiResponses] = useState<unknown[]>([])
+
+  const examples = [
+    {
+      name: 'Create quote | General information',
+      run: async () => {
+        const newStaticQuoteProperties = {
+          lobCd: 'HOME',
+          policyTerm: 1,
+          policyTermUnit: 'YR',
+          quoteStatus: 'QuoteIncomplete',
+          systemOfOriginVendor: 'Vermont Mutual',
+          systemOfOriginProduct: 'FrontEnd',
+          systemOfOriginVersion: '2.1',
+        }
+
+        const newQuoteProperties = {
+          createdBy: apiOptions.userId,
+          updatedBy: apiOptions.userId,
+          ...newStaticQuoteProperties,
+        }
+
+        const uiQuote = {
+          isoProgram: '91',
+          effectiveDt: '2020-12-05',
+          contractNumber: '20300',
+          niprId: '0005411111',
+          controllingStateProvCd: 'ME',
+          policyTypeCd: 'HO0001',
+          naicCd: '26018',
+          isBookTransfer: false,
+          ...newQuoteProperties,
+        }
+
+        const apiQuote = apiQuoteFromUiQuote(uiQuote)
+
+        setUiRequests(uiRequests => {
+          const cloned = [...uiRequests]
+          cloned[0] = uiQuote
+          return cloned
+        })
+
+        setApiRequests(apiRequests => {
+          const cloned = [...apiRequests]
+          cloned[0] = apiQuote
+          return cloned
+        })
+
+        const { data: apiQuoteResponse } = await request({
+          url: '/quote',
+          method: 'POST',
+          baseURL: apiUrl,
+          headers: {
+            userId: apiOptions.userId,
+            transactionId: apiOptions.transactionId,
+            Authorization: `bearer ${apiOptions.userId}`,
+            'Content-Type': 'application/json',
+          },
+          data: {
+            quote: apiQuote,
+            action: 'HOME_GEN_QUOTE_INFO',
+            userId: apiOptions.transactionId,
+            transactionId: apiOptions.transactionId,
+          },
+        })
+
+        setUiResponses(uiResponses => {
+          const cloned = [...uiResponses]
+          cloned[0] = apiQuoteResponse
+          return cloned
+        })
+
+        const uiQuoteResponse = uiQuoteFromApiQuote(apiQuoteResponse)
+
+        setApiResponses(apiResponses => {
+          const cloned = [...apiResponses]
+          cloned[0] = apiQuoteResponse
+          return cloned
+        })
+      },
+    },
+  ]
   return (
     <>
-      <Box paddingBottom={4}>
-        <Box paddingBottom={2}>
-          <Typography variant='h1'>
-            Create quote | General information
-          </Typography>
-        </Box>
-        <Box display='flex'>
-          {uiRequests[0] && (
-            <Box paddingBottom={2}>
-              <pre>{JSON.stringify(cleanOutput(uiRequests[0]), null, 2)}</pre>
-            </Box>
-          )}
-          {apiRequests[0] && (
-            <Box paddingBottom={2}>
-              <pre>{JSON.stringify(cleanOutput(apiRequests[0]), null, 2)}</pre>
-            </Box>
-          )}
-          {uiResponses[0] && (
-            <Box paddingBottom={2}>
-              <pre>{JSON.stringify(cleanOutput(uiResponses[0]), null, 2)}</pre>
-            </Box>
-          )}
-          {apiResponses[0] && (
-            <Box paddingBottom={2}>
-              <pre>{JSON.stringify(cleanOutput(apiResponses[0]), null, 2)}</pre>
-            </Box>
-          )}
-        </Box>
-        <Button
-          variant='outlined'
-          onClick={async () => {
-            const newQuote = {
-              isoProgram: '91',
-              effectiveDt: '2020-12-05',
-              contractNumber: '20300',
-              niprId: '0005411111',
-              controllingStateProvCd: 'ME',
-              policyTypeCd: 'HO0001',
-              naicCd: '26018',
-              isBookTransfer: false,
-            }
-            const apiQuote = prepareNewQuote(newQuote, apiOptions.userId)
-            setUiRequests(uiRequests => {
-              const cloned = [...uiRequests]
-              cloned[0] = apiQuote
-              return cloned
-            })
-            setApiRequests(apiRequests => {
-              const cloned = [...apiRequests]
-              cloned[0] = apiQuote
-              return cloned
-            })
-            const { data } = await request({
-              url: '/quote',
-              method: 'POST',
-              baseURL: apiUrl,
-              headers: {
-                userId: apiOptions.userId,
-                transactionId: apiOptions.transactionId,
-                Authorization: `bearer ${apiOptions.userId}`,
-                'Content-Type': 'application/json',
-              },
-              data: {
-                quote: apiQuote,
-                action: 'HOME_GEN_QUOTE_INFO',
-                userId: apiOptions.transactionId,
-                transactionId: apiOptions.transactionId,
-              },
-            })
-            setUiResponses(uiResponses => {
-              const cloned = [...uiResponses]
-              cloned[0] = data
-              return cloned
-            })
-            setApiResponses(apiResponses => {
-              const cloned = [...apiResponses]
-              cloned[0] = data
-              return cloned
-            })
-          }}
-        >
-          Run example
-        </Button>
-      </Box>
-      <Box paddingBottom={4}>
-        <Box paddingBottom={2}>
-          <Typography variant='h1'>
-            Update quote | General information
-          </Typography>
-        </Box>
-        <Box display='flex'>
-          {uiRequests[1] && (
-            <Box paddingBottom={2}>
-              <pre>{JSON.stringify(cleanOutput(uiRequests[1]), null, 2)}</pre>
-            </Box>
-          )}
-          {apiRequests[1] && (
-            <Box paddingBottom={2}>
-              <pre>{JSON.stringify(cleanOutput(apiRequests[1]), null, 2)}</pre>
-            </Box>
-          )}
-          {uiResponses[1] && (
-            <Box paddingBottom={2}>
-              <pre>{JSON.stringify(cleanOutput(uiResponses[1]), null, 2)}</pre>
-            </Box>
-          )}
-          {apiResponses[1] && (
-            <Box paddingBottom={2}>
-              <pre>{JSON.stringify(cleanOutput(apiResponses[1]), null, 2)}</pre>
-            </Box>
-          )}
-        </Box>
-        <Button
-          variant='outlined'
-          onClick={async () => {
-            const newQuote = {
-              isoProgram: '91',
-              effectiveDt: '2020-12-05',
-              contractNumber: '20300',
-              niprId: '0005411111',
-              controllingStateProvCd: 'ME',
-              policyTypeCd: 'HO0001',
-              naicCd: '26018',
-              isBookTransfer: false,
-            }
-            const uiQuote = prepareNewQuote(newQuote, apiOptions.userId)
-            setUiRequests(uiRequests => {
-              const cloned = [...uiRequests]
-              cloned[1] = uiQuote
-              return cloned
-            })
-            setApiRequests(apiRequests => {
-              const cloned = [...apiRequests]
-              cloned[1] = uiQuote
-              return cloned
-            })
-            const { data } = await request({
-              url: '/quote',
-              method: 'POST',
-              baseURL: apiUrl,
-              headers: {
-                userId: apiOptions.userId,
-                transactionId: apiOptions.transactionId,
-                Authorization: `bearer ${apiOptions.userId}`,
-                'Content-Type': 'application/json',
-              },
-              data: {
-                quote: uiQuote,
-                action: 'HOME_GEN_QUOTE_INFO',
-                userId: apiOptions.transactionId,
-                transactionId: apiOptions.transactionId,
-              },
-            })
-            setUiResponses(uiResponses => {
-              const cloned = [...uiResponses]
-              cloned[1] = data
-              return cloned
-            })
-            setApiResponses(apiResponses => {
-              const cloned = [...apiResponses]
-              cloned[1] = data
-              return cloned
-            })
-          }}
-        >
-          Run example
-        </Button>
-      </Box>
+      {examples.map((example, position) => (
+        <Example
+          key={position}
+          name={example.name}
+          onRun={example.run}
+          uiRequest={uiRequests[position]}
+          uiResponse={uiResponses[position]}
+          apiRequest={apiRequests[position]}
+          apiResponse={apiResponses[position]}
+        />
+      ))}
     </>
   )
 }
