22c93095e3a70fb904be7b6b6e064d86136bdf83
Mike Bannister <notimpossiblemike@gmail.com>
2021-03-07 17:29:17 -0500

    Use dark theme

--- modules/theme.ts
+++ modules/theme.ts
@@ -1,17 +1,24 @@
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 
 const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
   typography: {
     fontFamily: '"Roboto Mono", monospace',
   },
   overrides: {
     MuiTypography: {
       h1: {
-        fontSize: '1.2rem',
+        fontSize: '1.6rem',
         fontWeight: 'bold',
       },
       h2: {
-        fontSize: '1.0rem',
+        fontSize: '1.4rem',
+        fontWeight: 'bold',
+      },
+      h3: {
+        fontSize: '1.2rem',
         fontWeight: 'bold',
       },
     },
